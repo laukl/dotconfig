5650ab83bfd2a3425e346abe63972d98fab7b208
Mikey Lau <mikey@3sidedcube.com>
2024-08-08 01:14:38 +0100

    fix(xinitrc): setup script force ln

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-ln -s ./generic/.xinitrc $HOME/.xinitrc
+ln -fs ./generic/.xinitrc $HOME/.xinitrc
 
-ln -s ./kitty $HOME/.config/kitty
-ln -s ./nvim $HOME/.config/nvim
-ln -s ./polybar $HOME/.config/polybar
+ln -fs ./kitty $HOME/.config/kitty
+ln -fs ./nvim $HOME/.config/nvim
+ln -fs ./polybar $HOME/.config/polybar
