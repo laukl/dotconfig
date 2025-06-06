68a95dae7ab40125ba1a958cbd02feae9a4c99e2
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-06-06 11:03:41 +0100

    chore: remove avante

--- nvim/lua/mikeylau/plugins/lsp/mason.lua
+++ nvim/lua/mikeylau/plugins/lsp/mason.lua
@@ -1,37 +1,11 @@
 return {
 	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
+	dependencies = { "williamboman/mason-lspconfig.nvim" },
 	config = function()
 		local mason = require("mason")
-
 		local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
-
 		mason.setup()
-
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"graphql",
-				"prismals",
-				"helm_ls",
-			},
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier",
-				"stylua",
-				"tflint",
-				"tfsec",
-			},
-		})
+		mason_lspconfig.setup()
 	end,
 }
