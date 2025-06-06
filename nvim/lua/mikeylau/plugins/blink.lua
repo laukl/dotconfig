67a61ff7c958a58f92afd2b135d23de0c0034cc0
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-06-07 00:13:55 +0100

    chore: update blink config

--- nvim/lua/mikeylau/plugins/blink.lua
+++ nvim/lua/mikeylau/plugins/blink.lua
@@ -3,32 +3,28 @@ return {
 	version = "1.*",
 	opts = {
 		keymap = {
-			preset = "default",
-			["<S-tab>"] = { "select_prev", "fallback" },
-			["<tab>"] = { "select_next", "fallback" },
-			["<CR>"] = { "accept" },
-		},
+			preset = "none",
+			["<C-space>"] = { "show" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
 
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
+			["<Tab>"] = { "select_next", "fallback_to_mappings" },
+
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		},
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
-
-		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
-
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		completion = { documentation = { auto_show = true } },
 		sources = {
 			default = { "lsp", "path", "buffer" },
 		},
-
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 	opts_extend = { "sources.default" },
