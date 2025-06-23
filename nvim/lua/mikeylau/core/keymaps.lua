200a3776403d2ef4df2a7c98e27016b29a8259b1
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-06-23 13:17:09 +0100

    feat: add keymap for fzf buffers

--- nvim/lua/mikeylau/core/keymaps.lua
+++ nvim/lua/mikeylau/core/keymaps.lua
@@ -3,15 +3,5 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":nohl<cr>", { desc = "Clear search highlights" })
-keymap.set("n", "<leader>q", ":qa<cr>", { desc = "Quit all" })
 
-vim.keymap.set("n", "<leader>fg", function()
-	local pattern = vim.fn.input("rg: ")
-	if pattern ~= "" then
-		local safe_pattern = vim.fn.shellescape(pattern)
-		vim.cmd("silent! grep " .. safe_pattern)
-		vim.cmd("copen")
-	end
-end, { desc = "ripgrep" })
-
-vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "Code Action" })
+keymap.set("n", "<leader>.", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "Code Action" })
--- nvim/lua/mikeylau/plugins/fzf.lua
+++ nvim/lua/mikeylau/plugins/fzf.lua
@@ -7,5 +7,6 @@ return {
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>ff", "<cmd>Fzf files<cr>", { desc = "fzf files" })
 		keymap.set("n", "<leader>fg", "<cmd>Fzf live_grep<cr>", { desc = "fzf live_grep" })
+		keymap.set("n", "<leader>fb", "<cmd>Fzf buffers<cr>", { desc = "fzf buffers" })
 	end,
 }
