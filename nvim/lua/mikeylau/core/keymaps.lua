36e7e36c8b392a3cb72a96cc5bc7e57a002ae4db
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-06-23 13:47:54 +0100

    feat: add quit all keymap

--- nvim/lua/mikeylau/core/keymaps.lua
+++ nvim/lua/mikeylau/core/keymaps.lua
@@ -3,5 +3,6 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":nohl<cr>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>qq", ":qa<cr>", { desc = "Quit all" })
 
 keymap.set("n", "<leader>.", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "Code Action" })
